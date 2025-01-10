cf944e207e65cf13c05f1c47cd8bd3d336ea6aed
Wanderer <andrey.sn41@gmail.com>
2025-01-10 12:17:00 +0500

    читаем файл

---
+++ src/читаемФайл.kt
@@ -0,0 +1,51 @@
+import java.io.File
+
+fun main() {
+    //переменная file, нужно импортировать класс File
+    val file: File
+
+    //а так создаём объект класса File, для этого вызываем конструктор класса и передаём ему строку в которой описан путь к файлу
+    File("resources/Wizard.txt")
+    //здесь экземпляр класса File создан, но никак не использован
+
+    //Инициализируем переменную, помещаем в неё экземпляр, созданный конструктором File(...)
+    file = File("resources/Wizard.txt")
+
+    //теперь читаем файл построчно
+
+
+    //для каждой строки line вызывается метод workLine(String)
+    file.forEachLine( charset = Charsets.UTF_8, ::workLine )
+
+    //то же самое, но код обработки строки пишем в лямбде
+    file.forEachLine( charset = Charsets.UTF_8 ){ line ->
+        //выводим строку на экран консоли
+        println(line)
+    }
+
+    //то же самое, но используем кодировку символов по умолчанию, код обработки строки пишем в лямбде
+    file.forEachLine(){ line ->
+        //выводим строку на экран консоли
+        println(line)
+    }
+
+    //читаем все строки файла в список lines
+    val lines: List<String> = file.readLines()
+
+    //выводим список на экран в цикле
+    for(line in lines){
+        println(line)
+    }
+
+    //или так
+    lines.forEach { line ->
+        println(line)
+    }
+
+}
+
+fun workLine(line:String){
+    //выводим строку на экран консоли
+    println(line)
+}
+
